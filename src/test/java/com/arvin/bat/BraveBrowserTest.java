b5f40f7982046dcceb4fc9664fddc4c8896d194b
arvin <arvin@pop-os.localdomain>
2021-02-07 08:20:02 +0800

    bat test commit

--- src/test/java/com/arvin/bat/BraveBrowserTest.java
+++ src/test/java/com/arvin/bat/BraveBrowserTest.java
@@ -1,2 +1,21 @@
-package com.arvin.bat;public class BraveBrowserTest {
+package com.arvin.bat;
+
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class BraveBrowserTest {
+
+    @Test
+    public void testGetUrls() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        BraveBrowser browser = new BraveBrowser("");
+        Method getUrls = BraveBrowser.class.getDeclaredMethod("getUrls");
+        boolean origAccessible = getUrls.isAccessible();
+        getUrls.setAccessible(true);
+        List<String> urls = (List<String>) getUrls.invoke(browser);
+        getUrls.setAccessible(origAccessible);
+        assert urls != null && urls.size() > 0 && urls.size() < 3092;
+    }
 }
--- src/test/java/com/arvin/bat/ReentrantLockTest.java
+++ src/test/java/com/arvin/bat/ReentrantLockTest.java
@@ -1,2 +1,22 @@
-package com.arvin.bat;public class ReentrantLockTest {
+package com.arvin.bat;
+
+import org.junit.Test;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockTest {
+
+    @Test
+    public void test() {
+        ReentrantLock lock = new ReentrantLock(true);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                lock.lock();
+            }
+        }).start();
+        lock.lock();
+    }
 }
